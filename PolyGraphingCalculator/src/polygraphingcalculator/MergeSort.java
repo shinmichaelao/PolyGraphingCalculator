38dd185c4347a3ed1ccca85fb9040df9d6907b37
shinmichaelao <michaelxuao@gmail.com>
2016-11-10 20:59:37 -0500

    RIP Failed to commit everything last time

--- PolyGraphingCalculator/src/polygraphingcalculator/MergeSort.java
+++ PolyGraphingCalculator/src/polygraphingcalculator/MergeSort.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package polygraphingcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Michael
- */
+
 public class MergeSort {
     
     public static List<Term> mergeSort(List<Term> a){
