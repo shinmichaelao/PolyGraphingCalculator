d46b7754aacdc0cc768c5398e209d71be90247c9
shinmichaelao <michaelxuao@gmail.com>
2016-11-10 20:59:06 -0500

    Removed the garbage headers and shit

--- PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
+++ PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package polygraphingcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**
- *
- * @author Michael
- */
+
 public class PolyGraphingCalculator {
 
     
