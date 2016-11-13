57e114efde09599fa18f0c4164a3d067750de562
shinmichaelao <michaelxuao@gmail.com>
2016-11-12 20:02:24 -0500

    DONE STRING TO POLYNOMIAL

--- PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
+++ PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
@@ -10,17 +10,19 @@ public class PolyGraphingCalculator {
 
     
     public static void main(String[] args) {
-//        Random rn = new Random(); //testing the sorting
-//        List<Term> array = new ArrayList();
-//        for (int i = 0; i < 10; i++) {
-//            array.add(new Term(rn.nextInt(10),rn.nextInt(3)));
-//        }
-//        
-//        Polynomial testPoly = new Polynomial(array);
-//        testPoly.printPolynomial();
-//        testPoly.sortByDegree();
-//        testPoly.printPolynomial();
-        Polynomial kappa = new Polynomial("50.2x");
+        Random rn = new Random(); //testing the sorting
+        List<Term> array = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            array.add(new Term(rn.nextInt(10),rn.nextInt(3)));
+        }
+        
+        Polynomial testPoly = new Polynomial(array); //sorting opop
+        testPoly.printPolynomial();
+        testPoly.sortByDegree();
+        testPoly.printPolynomial();
+
+        Polynomial kappa = new Polynomial("2x^2 - 5 + 12341 + 5x^2 - 8"); //testing string to polynomial
+        kappa.printPolynomial();
     }
     
 }
