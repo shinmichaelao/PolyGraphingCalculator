4e8cb7fd572d2bff25cdbbabbb09f2f40177c021
shinmichaelao <michaelxuao@gmail.com>
2016-11-12 19:24:45 -0500

    Almost reads 1 term lmao

--- PolyGraphingCalculator/src/polygraphingcalculator/Polynomial.java
+++ PolyGraphingCalculator/src/polygraphingcalculator/Polynomial.java
@@ -13,10 +13,36 @@ public class Polynomial {
     }
     
     public Polynomial(String s){
-        double expo
-        for(int i = 0; i < s.length(); i ++){
+        //the variable i will be the current pointer
+        int i = 0;
+        //while(i < s.length()){
+            String coeff = "0";
+            String degree = "0";
+            String cur_char = "imapotato";
             
-        }
+            //get the coefficient
+            while(i < s.length()){
+                cur_char = s.substring(i,i+1);
+                if(cur_char.equals("x") || cur_char.equals("+") || cur_char.equals("-") || cur_char.equals(" ")){
+                    break;
+                }
+                coeff += cur_char;
+                i++;
+                System.out.println("pos: "+i);
+            }
+            System.out.print(Double.parseDouble(coeff));
+            
+            //check if there is an "x" portion, otherwise coefficient is 0 like before
+            if(cur_char.equals("x")){
+                //if theres nothing after the x, then the degree is 1
+                if(i == s.length()-1){
+                    degree = "1";
+                }
+                //check the next few characters until we get to a " ", "+","-"
+            }
+            
+            System.out.println("x^" + Integer.parseInt(degree));
+       // }
     }
     public void printPolynomial(){
         System.out.println("");
