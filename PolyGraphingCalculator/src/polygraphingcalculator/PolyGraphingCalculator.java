e4fba78664944c3ac7bdab706a19e84ad5ec3433
shinmichaelao <michaelxuao@gmail.com>
2016-11-20 18:33:18 -0500

    commented out the test garbo

--- PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
+++ PolyGraphingCalculator/src/polygraphingcalculator/PolyGraphingCalculator.java
@@ -4,8 +4,8 @@ public class PolyGraphingCalculator {
     static final double maxValue = Math.pow(2, 31)-1;
     
     public static void main(String[] args) {
-        Polynomial test1 = new Polynomial("5x - 50x^2");
-        System.out.println(test1.toString());
+       // Polynomial test1 = new Polynomial("5x - 50x^2");
+        //System.out.println(test1.toString());
         //Polynomial test2 = new Polynomial(" -10x + 20x + 15");
        // System.out.println(test2.toString());
         GUI.main(args);
